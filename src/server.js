6e566c81977d31eb7bba44181947ce65865dd9b5
Danny Chen <danny.chen.3@stonybrook.edu>
2024-02-22 18:39:57 -0500

    creating a peer node

--- src/server.js
+++ src/server.js
@@ -1,9 +1,3 @@
-const Libp2p = require('libp2p');
-const TCP = require('@libp2p/tcp');
-const MPLEX = require('@libp2p/mplex');
-const { NOISE } = require('@chainsafe/libp2p-noise');
-const MDNS = require('@libp2p/mdns');
-
 class PeerNode {
   constructor(id, ipAddress, portNumber, walletAddress) {
     this.id = id; //unique identifer for the peer
@@ -11,39 +5,6 @@ class PeerNode {
     this.portNumber = portNumber; //Port number for peer
     this.walletAddress = walletAddress; //blockchain wallet address for transactions
     this.lastSeen = Date.now(); //timestamp for last known activity
-    this.libp2p = null;
-  }
-
-  async start() {
-    this.libp2p = await Libp2p.create({
-      addresses: {
-        listen: [`${this.ipAddress}/tcp/${this.portNumber}`] 
-      },
-      modules: {
-        transport: [TCP],
-        streamMuxer: [MPLEX],
-        connEncryption: [NOISE],
-        peerDiscovery: [MDNS]
-      }
-    });
-
-    // Event listeners for libp2p
-    this.libp2p.on('peer:discovery', (peerId) => {
-      console.log(`Discovered: ${peerId.toB58String()}`);
-    });
-
-    this.libp2p.connectionManager.on('peer:connect', (connection) => {
-      console.log(`Connected to: ${connection.remotePeer.toB58String()}`);
-      this.updateLastSeen();
-    });
-
-    // Start the libp2p node
-    await this.libp2p.start();
-    console.log('Libp2p started');
-  }
-  
-  updateLastSeen() {
-    this.lastSeen = Date.now();
   }
 }
 
