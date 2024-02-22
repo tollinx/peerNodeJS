7fd05300f25abc1e1cc489b0b4595cc32c612720
Tony Lin <tonylin@Tonys-MacBook-Pro.local>
2024-02-22 18:37:20 -0500

    Restructured project

---
+++ src/ledger/blockchainClient.js
@@ -0,0 +1,3 @@
+// Interacts with the blockchain??
+// How will we interact with the blockchain?
+// Or the market?
--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ export class PeerNode {
     this.ipAddress = ipAddress; //IP address of peer
     this.portNumber = portNumber; //Port number for peer
     this.walletAddress = walletAddress; //blockchain wallet address for transactions
-    this.lastSeen = Date.now(); //timestamp for last known activity
     this.libp2p = null;
   }
 
@@ -46,37 +45,6 @@ export class PeerNode {
     this.lastSeen = Date.now();
   }
 }
-
-export function getNodes() {
-  return [...servers];
-}
-
-export function addNode(node) {
-  console.log(`registering ${node.user}`);
-  const isAlreadyAdded = servers.find(existingNode => existingNode.user === node.user);
-  if (isAlreadyAdded) return;
-  servers.push(node);
-}
-
-export function getNodeByUser(user) {
-  return servers.find(server => server.user === user);
-}
-class PeerNode {
-  constructor(id, ipAddress, portNumber) {
-    this.id = id;
-    this.ipAddress = ipAddress;
-    this.portNumber = portNumber;
-  }
-}
-
-export class Node {
-  constructor(uri, user) {
-    this.uri = uri;
-    this.user = user;
-  }
-}
-
-const servers = [];
 
 export function getNodes() {
   return [...servers];
