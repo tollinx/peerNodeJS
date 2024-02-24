230348049185ed71c3c7ab3ff1efd5481177683e
Tony Lin <tonylin@Tonys-MacBook-Pro.local>
2024-02-24 05:35:25 -0500

    Fixed address bootrap bug. The bootstrap node can now successfully connect to ip address and port without timing out. Can use dig to find more bootstrap nodes. Refactored the code and started increasing additional functionality for nodes.

---
+++ libp2p.js
@@ -0,0 +1,136 @@
+import process from 'node:process'
+import { createLibp2p } from 'libp2p'
+import { tcp } from '@libp2p/tcp'
+// import { websockets } from '@libp2p/websockets'
+import { noise } from '@chainsafe/libp2p-noise'
+import { multiaddr } from 'multiaddr'
+import { kadDHT } from '@libp2p/kad-dht'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { ping } from '@libp2p/ping' // remove this after done testing
+import { bootstrap } from '@libp2p/bootstrap'
+
+async function main() {
+
+        // Can manage creation of nodes here
+        // For example, subscribe to events, handle incoming messages, etc.
+
+    const node = await createLibp2p({
+        addresses: {
+            // add a listen address (localhost) to accept TCP connections on a random port
+            listen: ['/ip4/0.0.0.0/tcp/0']
+        },
+        transports: [
+            tcp()
+        ],
+        streamMuxers: [
+            yamux()
+        ],
+        connectionEncryption: [
+            noise()
+        ],
+        peerDiscovery: [
+            bootstrap({
+                list: [
+                    // bootstrap node here is generated from dig command
+                    '/dnsaddr/sg1.bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+                ]
+            })
+        ],
+        services: {
+            dht: kadDHT({
+                kBucketSize: 20,
+            }),
+            ping: ping({
+                protocolPrefix: 'ipfs',
+            }),
+        }
+    })
+
+
+
+    // node.connectionManager.on('peer:connect', (connection) => {
+    //     console.info(`Connected to ${connection.remotePeer.toB58String()}!`)
+    // })
+
+    startNode(node)
+
+    console.log('listening on addresses: ')
+    node.getMultiaddrs().forEach((addr) => {
+        console.log(addr.toString())
+    })
+
+
+    // Retrieve ip address of a bootstrap node:
+    // dig -t TXT _dnsaddr.bootstrap.libp2p.io
+    const targetAddress = multiaddr('/dnsaddr/sg1.bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt')
+    try {
+        await node.dial(targetAddress)
+        stopNode(node)
+    } catch (err) {
+        console.error(err)
+    }
+
+    // Forcefully quit main
+    // process.on('SIGTERM', stop);
+    // process.on('SIGINT', stop);
+}
+
+async function startNode(node) {
+    console.log("Starting node: ")
+    await node.start();
+}
+
+async function stopNode(node) {
+    console.log("Stopping node: ")
+    await node.stop();
+}
+
+// Connecting a node to all the peers in its network
+// may want to add another parameter "neighbors" to restrict what nodes it can access
+async function discoverPeers(node) {
+    // Implement peer discovery mechanisms here
+    // For example, using bootstrap nodes or mDNS
+    try {
+        // Use dig to find other examples of bootstrap node addresses
+        // we can assume we have these already, hence they're hardcoded
+        const bootstrapNodes = [
+            '/dns4/bootstrap.libp2p.io/tcp/443/wss/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+            '/dns4/bootstrap.libp2p.io/tcp/443/wss/p2p/QmZvFnUfyFxkfzfjN7c1j6E1YKgKvZgoCyzp4TD5Yk3BdU'
+        ];
+
+        // Connect to each bootstrap node to discover more peers
+        for (const addr of bootstrapNodes) {
+            const ma = multiaddr(addr);
+            await node.dial(ma);
+        }
+
+    } catch (error) {
+        console.error('Peer discovery failed:', error);
+    }
+}
+
+async function routeMessage(node, message, targetPeerId) {
+    // Route the message to the specified target peer
+}
+
+// need to read more into pub sub testing protocols
+async function exchangeData(node, peerId, data) {
+    // Implement data exchange protocol here
+    // Send and receive data with the specified peer
+    try {
+        // Publish data to a topic
+        await node.pubsub.publish(topic, data);
+        console.log('Data published:', data);
+
+        // Subscribing means this node will receive notifs
+        await node.pubsub.subscribe(topic, (message) => {
+            console.log('Received data:', message.data.toString());
+        });
+        console.log('Subscribed to topic:', topic);
+
+    } catch (error) {
+        console.error('Data exchange failed:', error);
+    }
+}
+
+main()
---
+++ src/index.js
@@ -0,0 +1,24 @@
+class PeerNode {
+    constructor(id, ipAddress, portNumber, walletAddress) {
+      this.id = id; //unique identifer for the peer
+      this.ipAddress = ipAddress; //IP address of peer
+      this.portNumber = portNumber; //Port number for peer
+      this.walletAddress = walletAddress; //blockchain wallet address for transactions
+      this.lastSeen = Date.now(); //timestamp for last known activity
+    }
+  }
+  
+  export function getNodes() {
+    return [...servers];
+  }
+  
+  export function addNode(node) {
+    console.log(`registering ${node.user}`);
+    const isAlreadyAdded = servers.find(existingNode => existingNode.user === node.user);
+    if (isAlreadyAdded) return;
+    servers.push(node);
+  }
+  
+  export function getNodeByUser(user) {
+    return servers.find(server => server.user === user);
+  }
